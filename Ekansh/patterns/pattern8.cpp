791785992cb385ac7376e9c877008e3d92c13b22
Ekanshk10 <156538370+Ekanshk10@users.noreply.github.com>
2026-02-08 23:32:44 +0530

    10 patterns done

--- Ekansh/patterns/pattern8.cpp
+++ Ekansh/patterns/pattern8.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n = 5;
     for(int i =0; i<n; i++){
-        for(int k = 0; k<=i; k++){
+        for(int k = 0; k<i; k++){
             cout<<" ";
         }
         for(int j = 0; j<2*n-2*i-1; j++){
