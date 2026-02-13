6c3b47fb6cf69408f2470a41bf4b4b1a4c4b29cb
Ekanshk10 <156538370+Ekanshk10@users.noreply.github.com>
2026-02-13 23:56:52 +0530

    optimized Bubble sort adding a check to break the loop if array is already sorted

--- Ekansh/sorting/bubble.cpp
+++ Ekansh/sorting/bubble.cpp
@@ -15,15 +15,27 @@ int main(){
 
     cout<<endl;
 
+    // for(int i = n-1; i>0; i--){
+    //     for(int j = 0; j<=i-1; j++){
+    //         if(arr[j]>arr[j+1]){
+    //             int temp = arr[j];
+    //             arr[j] = arr[j+1];
+    //             arr[j+1] = temp;
+
+    //         }
+    //     }
+    // }
     for(int i = n-1; i>0; i--){
+        int didSwap = 0;
         for(int j = 0; j<=i-1; j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-
+                didSwap = 1;
             }
         }
+        if(didSwap == 0) break;
     }
     cout<<"After Sorting: ";
     for(int i=0; i<n; i++){
