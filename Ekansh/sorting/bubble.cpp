62731617722a53b2ce33b15d515dbf771020ea5e
Ekanshk10 <156538370+Ekanshk10@users.noreply.github.com>
2026-02-15 23:12:43 +0530

    recursive bubble and insertion sort done

--- Ekansh/sorting/bubble.cpp
+++ Ekansh/sorting/bubble.cpp
@@ -1,44 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<"Before Sorting: ";
-    for(int i = 0; i<n ; i++){
-        cout<<arr[i];
+    cout << "Before Sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
     }
 
-    cout<<endl;
+    cout << endl;
 
-    // for(int i = n-1; i>0; i--){
-    //     for(int j = 0; j<=i-1; j++){
-    //         if(arr[j]>arr[j+1]){
-    //             int temp = arr[j];
-    //             arr[j] = arr[j+1];
-    //             arr[j+1] = temp;
-
-    //         }
-    //     }
-    // }
-    for(int i = n-1; i>0; i--){
+    for (int i = n - 1; i > 0; i--)
+    {
         int didSwap = 0;
-        for(int j = 0; j<=i-1; j++){
-            if(arr[j]>arr[j+1]){
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
                 didSwap = 1;
             }
         }
-        if(didSwap == 0) break;
+        if (didSwap == 0)
+            break;
     }
-    cout<<"After Sorting: ";
-    for(int i=0; i<n; i++){
-        cout<<arr[i];
+    cout << "After Sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
     }
 }
+// recursive
+/*
+void bubble_sort(int arr[], int n) {
+    if (n == 1) return;
+
+    int didSwap = 0;
+
+    for (int j = 0; j <= n - 2; j++) {
+        if (arr[j] > arr[j + 1]) {
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            didSwap = 1;
+        }
+    }
+
+    if (didSwap == 0) return;
+    bubble_sort(arr, n - 1);
+}
+*/
--- Ekansh/sorting/insertion.cpp
+++ Ekansh/sorting/insertion.cpp
@@ -1,24 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i= 0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    for(int i = 0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int j = i;
-        while(j>0 && arr[j-1]> arr[j]){
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
             int temp = arr[j];
-            arr[j] = arr[j-1];
-            arr[j-1] = temp;
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
             j--;
         }
     }
-    cout<<"After sorting: ";
-    for(int i = 0; i<n; i++){
-        cout<<arr[i];
+    cout << "After sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
     }
 }
+
+// recursion
+/*
+void insertion_sort(int arr[], int i, int n) {
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
+*/
